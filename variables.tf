1f515b2528860fc84d17d89097df35fb3e542346
srinivasyadav552 <srinivasyadav552@gmail.com>
2021-10-12 10:19:09 +0530

    MyfirstTerraformModule

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "accesskey" {
+	default="" 
+}
+
+variable "secretkey" {
+	default=""
+}
+
+variable "keypair" {
+	default="Ec-2"
+}
+
+variable "ami-id" {
+	default="ami-06a0b4e3b7eb7a300"
+}
+
+variable "vpcID" {
+	default="vpc-a85da1c3"
+}
+
+
+
+
